f616cf82b3121cdbe0d1a098e022ae520226fdc4
unk1nd <46972799+thereeasd@users.noreply.github.com>
2022-01-06 22:27:24 +0600

    new solution

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	int N, M, K;
+	cin >> N >> M >> K;
+	if ((N * M) > K && ((K % M) == 0 || (K % N) == 0))
+	{
+		cout << "YES";
+	}
+	else cout << "NO";
+	return 0;
+}
