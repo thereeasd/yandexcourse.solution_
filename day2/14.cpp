63270c5e3b4960d7aff7a3c920801173166f40cc
unk1nd <46972799+thereeasd@users.noreply.github.com>
2022-01-07 18:49:09 +0600

    last ex + solution swap function

--- day2/14.cpp
+++ day2/14.cpp
@@ -57,3 +57,26 @@ int main() {
     else cout << "Boxes are incomparable";
     return 0;
 }
+
+/*
+Решение аналогичное предыдущему используя swap, на момент решения курса мне был известен, но на пороге вхождения по решению курса не известен студенту.
+#include <iostream>
+using namespace std;
+int main() {
+    int a1, b1, c1, a2, b2, c2;
+    cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+    if (a1>b1) swap (a1,b1);
+    if (b1>c1) swap (b1,c1);
+    if (a1>b1) swap (a1,b1);
+    if (a2>b2) swap (a2,b2);
+    if (b2>c2) swap (b2,c2);
+    if (a2>b2) swap (a2,b2);
+    if (a1==a2 and b1==b2 and c1==c2) cout << "Boxes are equal";
+    else
+        if (a1>=a2 and b1>=b2 and c1>=c2) cout << "The first box is larger than the second one";
+        else 
+            if (a2>=a1 and b2>=b1 and c2>=c1) cout << "The first box is smaller than the second one";
+            else cout << "Boxes are incomparable";
+    return 0;
+}
+*/
